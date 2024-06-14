e524e270d6d07a40d40889d8b1e57ee95a47f768
NivZindorf <nivz123456@gmail.com>
2024-06-14 14:04:04 +0300

    created dedicated type for customdotprops

--- Trends/apps/ui-plugin/src/components/chart/Chart.tsx
+++ Trends/apps/ui-plugin/src/components/chart/Chart.tsx
@@ -1,8 +1,7 @@
 import {LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip} from 'recharts';
 
-import {CustomDot} from "./CustomDot";
+import {CustomDot, CustomDotProps} from "./CustomDot";
 import {ChartData} from "./chart.types";
-
 
 type ChartProps={
   data:ChartData[]
@@ -11,7 +10,7 @@ type ChartProps={
 export const Chart = ({data}:ChartProps) => {
   return (
       <LineChart width={300} height={200} data={data}  >
-        <Line type="monotone" dataKey="price" dot={<CustomDot/>}/>
+        <Line type="monotone" dataKey="price" dot={(props:CustomDotProps)=><CustomDot {...props}/>}/>
         <CartesianGrid strokeDasharray="5 5" />
         <XAxis dataKey="date" />
         <YAxis width={45} />
