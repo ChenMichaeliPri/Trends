ede2c9eada3154a2f732e78e9d298aab3049241c
NivZindorf <nivz123456@gmail.com>
2024-07-20 11:05:00 +0300

    change title to Price Pulse

--- Trends/apps/ui-plugin/src/components/mainPage/mainPage.constants.ts
+++ Trends/apps/ui-plugin/src/components/mainPage/mainPage.constants.ts
@@ -1,5 +1,5 @@
 export const MAIN_PAGE = {
-  TITLE_TEXT:'Price Trends',
+  TITLE_TEXT:'Price Pulse',
   SUBHEADER_TEXT:'Showing data for {productName}',
   TRENDS_BUTTON_TEXT:'Show Trends',
   INSIGHTS_BUTTON_TEXT:'Show Insights'
