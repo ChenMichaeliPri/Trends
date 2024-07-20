4c81ded9a2c906e102284bb294d56e6e03732881
NivZindorf <nivz123456@gmail.com>
2024-07-20 11:46:25 +0300

    fix linter errors

--- Trends/apps/ui-plugin/src/components/App.tsx
+++ Trends/apps/ui-plugin/src/components/App.tsx
@@ -3,11 +3,9 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {useState} from "react";
 
 import {MainPageController} from "./mainPage/MainPageController";
-import {useLocalStorage} from "../utils/localStorage/LocalStorageProvider";
 
 export const App = () => {
     const [queryClient] = useState(() => new QueryClient())
-    const {data} = useLocalStorage();
   return (
     <QueryClientProvider client={queryClient}>
       <Box sx={{display:'flex' ,height:'100%',width:'100%'}}>
